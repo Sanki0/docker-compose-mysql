b90ae1e7b8a321c42210005a46440c26951a0a1b
sanki0 <sebas_elias_999@hotmail.com>
2023-06-24 18:08:54 -0500

    wprks

--- database/migration/migration.sql
+++ database/migration/migration.sql
@@ -14,3 +14,34 @@ INSERT INTO `students` (`nombre`, `dni`,`direccion`,`fecha_nacimiento`) VALUES
        ('Adrian','1234124', 'Valle', '2020-01-01'),
        ('John', '1234124', 'Valle', '2020-01-01'),
        ('Mary', '1234124', 'Valle', '2020-01-01');
+
+CREATE TABLE `tipoCliente`
+(
+    idTipoCliente   bigint auto_increment,
+    nombre varchar(255) NOT NULL,
+    detalle varchar(255) NOT NULL,
+    PRIMARY KEY (`idTipoCliente`)
+);
+
+INSERT INTO `tipoCliente` (`nombre`, `detalle`) VALUES 
+       ('Cliente', 'Detalle para Cliente'),
+       ('Proveedor', 'Detalle para Proveedor'),
+       ('Empleado', 'Detalle para Empleado');
+
+CREATE TABLE `cliente`
+(
+    idCliente   bigint auto_increment,
+    nombre varchar(255) NOT NULL,
+    dni char(8) NOT NULL,
+    telefono varchar(255) NOT NULL,
+    correo varchar(255) NOT NULL,
+    estado char(1) NOT NULL,
+    idTipoCliente bigint NOT NULL,
+    PRIMARY KEY (`idCliente`),
+    FOREIGN KEY (`idTipoCliente`) REFERENCES `tipoCliente` (`idTipoCliente`)
+);
+
+INSERT INTO `cliente` (`nombre`, `dni`,`telefono`,`correo`,`estado`,`idTipoCliente`) VALUES 
+       ('sanki0', '12345678', '9886472', 'sebas@gmail.com', 'A', 1),
+       ('sanki1', '98565354', '4586472', 'sanki1@gmail.com', 'A', 1),
+       ('sanki2', '12676578', '9872471', 'sanki2@gmail.com', 'A', 2);
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 type Student struct {
@@ -17,37 +19,126 @@ type Student struct {
 	Fecha_nacimiento string `json:"fecha_nacimiento"`
 }
 
-func main() {
-	db, err := sql.Open("mysql", "test_user:secret@tcp(172.18.0.2:3306)/test_database")
+type TipoCliente struct {
+	IdTipoCliente int    `json:"idTipoCliente"`
+	Nombre        string `json:"nombre"`
+	Detalle       string `json:"detalle"`
+}
+
+type Cliente struct {
+	IdCliente     int    `json:"idCliente"`
+	Nombre        string `json:"nombre"`
+	Dni           string `json:"dni"`
+	Telefono      string `json:"telefono"`
+	Correo        string `json:"correo"`
+	Estado        string `json:"estado"`
+	IdTipoCliente int    `json:"idTipoCliente"`
+}
+
+func connectionDB() *sql.DB {
+	db, err := sql.Open("mysql", "test_user:secret@tcp(db:3306)/test_database")
 	if err != nil {
-		log.Fatal(err)
+		panic(err.Error())
 	}
+	return db
+}
+func chkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func PingDb(db *sql.DB) {
+	err := db.Ping()
+	chkError(err)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+}
+
+func getTipoClientes() ([]TipoCliente, error) {
+
+	db := connectionDB()
 	defer db.Close()
+	PingDb(db)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT id, nombre, dni, direccion, fecha_nacimiento FROM students")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer rows.Close()
+	statement := "SELECT * FROM tipoCliente"
+	rows, err := db.Query(statement)
+	chkError(err)
+	defer rows.Close()
 
-		for rows.Next() {
-			var id int
-			var nombre, dni, direccion, fechaNacimiento string
-			err := rows.Scan(&id, &nombre, &dni, &direccion, &fechaNacimiento)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			fmt.Fprintf(w, "ID: %d, Nombre: %s, DNI: %s, Dirección: %s, Fecha de Nacimiento: %s\n", id, nombre, dni, direccion, fechaNacimiento)
-		}
+	tipoClientes := []TipoCliente{}
 
-		if err := rows.Err(); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+	for rows.Next() {
+		var t TipoCliente
+		if err := rows.Scan(&t.IdTipoCliente, &t.Nombre, &t.Detalle); err != nil {
+			return nil, err
 		}
-	})
+		tipoClientes = append(tipoClientes, t)
+	}
+
+	return tipoClientes, nil
+}
+
+func getTipoClientesController(w http.ResponseWriter, r *http.Request) {
+
+	tipoClientes, err := getTipoClientes()
+	if err != nil {
+		fmt.Fprintf(w, "No tipo clientes found")
+	}
+	if tipoClientes != nil {
+		json.NewEncoder(w).Encode(tipoClientes)
+	}
+
+}
+
+func getClientes() ([]Cliente, error) {
+
+	db := connectionDB()
+	defer db.Close()
+	PingDb(db)
+
+	statement := "SELECT * FROM cliente"
+	rows, err := db.Query(statement)
+	chkError(err)
+
+	clientes := []Cliente{}
+
+	for rows.Next() {
+		var c Cliente
+		if err := rows.Scan(&c.IdCliente, &c.Nombre, &c.Dni, &c.Telefono, &c.Correo, &c.Estado, &c.IdTipoCliente); err != nil {
+			return nil, err
+		}
+		clientes = append(clientes, c)
+	}
+
+	return clientes, nil
+}
+
+func getClientesController(w http.ResponseWriter, r *http.Request) {
+
+	clientes, err := getClientes()
+	if err != nil {
+		fmt.Fprintf(w, "No clientes found")
+	}
+	if clientes != nil {
+		json.NewEncoder(w).Encode(clientes)
+	}
+}
+
+func main() {
+
+	r := mux.NewRouter()
+
+	// get tipo clientes
+	r.HandleFunc("/tipoClientes", getTipoClientesController).Methods("GET")
+
+	// get clientes
+	r.HandleFunc("/clientes", getClientesController).Methods("GET")
+
+	r.HandleFunc("/", homePage)
+	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
